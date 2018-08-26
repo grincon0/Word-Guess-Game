14d741f3eebd2b8d2ebb47662fd0204073f4c912
Wake <georgerincon2@gmail.com>
2018-08-26 02:45:24 -0400

    updated script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,43 +7,75 @@ function startGame() {
 }
 
 var game = {
-    wordBank : ["mario", "Snake", "Ness", "Megaman", "Zero", "Skull Kid"],
-    stage : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-    currentStage : 0,
-    wins : 0,
-    start : function () {
+    wordBank: ["mario", "Snake", "Ness", "Megaman", "Zero", "Skull Kid"],
+    id: ['a', 'b', 'c', 'd', 'e', 'f'],
+    indexx: 0,
+    stage: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    currentStage: 0,
+    wins: 0,
+    chances: 10,
+    guessCorrect: true,
+    howManyCorrect: 0,
+    start: function () {
         document.getElementById("pressAny").innerHTML = "Press any button to begin!";
-        this.interval = setInterval(this.updateGame2, 60);
+        document.getElementById('chancesLeft').innerHTML = "Guesses left : " + this.chances;
+        setInterval(this.updateStats, 60);
     },
-    updateGame : function(){
-        document.onkeyup = function (event) {
-            game.wordBank.forEach((word, i) => {
-                var compare = game.wordBank[game.currentStage];
-                for(var z = 0; z < compare.length; z++){
-                    console.log(compare.charAt([z]));
-                }
-
-
-            });
-        }
-    },
-    updateGame2: function() {
-        document.onkeyup = function (event) {
     
+    run: function () {
+        document.onkeyup = function (event) {
+            var checker = 0;
+            var correct;
+            var stageFinish;
+            var elemId = game.id[game.indexx];
             var compare = game.wordBank[game.currentStage];
+            var comLetters = compare.split('');
+            console.log(comLetters);
             for (var z = 0; z < compare.length; z++) {
-                console.log(compare.charAt([z]));
-                if(event.key === compare.charAt([z])){
+                // console.log(compare.charAt([z]));
+                if (event.key === compare.charAt([z])) {
+                    correct = true;
                     var letter = event.key;
-                    document.getElementById('test').innerHTML = letter;
+                    document.getElementById(elemId).innerHTML = letter;
+                    //this is doing this 4 times doe to invetal, set it outside
+                    game.howManyCorrect += 1;
+                    game.chances += -1;
+                    checker = 1;
+
+
+
+                } else {
+                    correct = false;
+                    game.chances += -1;
+
+
                 }
             }
-    
-    
-    
+            if (checker) {
+                game.indexx++;
+                //game.currentStage++;
+                checker = 0;
+            }
+            if ((game.indexx >= (compare.length - 1)) && (game.howManyCorrect >= compare.length)) {
+                game.indexx = 0;
+                game.currentStage++;
+            } else if (game.indexx < (compare.length - 1) && (game.chances = 0)) {
+                alert("GAMEOVER");
+            }
+
+
+
+
+
         }
+
+    },
+    updateStats : function (){
+        document.getElementById('chancesLeft').innerHTML = "Guesses left : " + game.chances;
+        document.getElementById('zhowManyCorrect').innerHTML = "Guesses left : " + game.howManyCorrect;
     }
 
 }
+game.run();
 
 
