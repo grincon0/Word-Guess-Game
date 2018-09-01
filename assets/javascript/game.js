6b4230b5a0fe2da60d226a60bde2d00f32fd3e85
Wake <georgerincon2@gmail.com>
2018-09-01 01:49:49 -0400

    overhauled logic and game properties

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,91 @@
-var guessBank = ["Mega Man X", "Star Fox", "Earthbound", "Metroid"];
-
+var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y', 'z'];
+var wordDiv = document.getElementById("pldiv");
+var chancesDiv = document.getElementById("chancesLeft");
 
 
 function startGame() {
     game.start();
 }
 
-var game = {
-    wordBank: ["mario", "snake", "ness", "megaman", "zero", "Skull Kid"],
-    id: ['a', 'b', 'c', 'd', 'e', 'f'],
-    indexx: 0,
-    stage: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-    currentStage: 0,
-    wins: 0,
-    chances: 10,
-    guessCorrect: true,
-    howManyCorrect: 0,
-    start: function () {
-        document.getElementById("pressAny").innerHTML = "Press any button to begin!";
-        document.getElementById('chancesLeft').innerHTML = "Guesses left : " + this.chances;
-        setInterval(this.updateStats, 60);
-    },
-
-    run: function () {
-        document.onkeyup = function (event) {
-            var checker = 0;
-            var correct = false;
-            var stageFinish;
-            var elemId = game.id[game.indexx];
-            var compare = game.wordBank[game.currentStage];
-            var comLetters = compare.split('');
-            console.log(comLetters);
-            for (var z = 0; z < comLetters.length; z++) {
-                // console.log(compare.charAt([z]));
-                if (event.key === comLetters[z]) {
-                    correct = true;
-                    var letter = event.key;
-                    //if word is more than 6 character, will throw a error bc they are only 6 spaces(ids) for the characters in the html doc
-                    document.getElementById(elemId).innerHTML = letter;
-                    checker = true;
-                } else {
-                    correct = false;
-                }
-            }
-            if (checker) {
-                game.indexx++;
-                checker = false;
-            }
-            
-
-            if (correct) {
-                game.howManyCorrect++;
-                game.chances--;
-            } else {
-                game.chances--;
-            }
-
-            if ((game.indexx) === (compare.length)) {
-                game.currentStage++;
-                game.indexx = 0;
-                game.chances = 10;
-                game.id.forEach((element, i) => {
-                    document.getElementById(element).innerHTML = '';
-                });
-                
-            } else if (game.chances === 0) {
-                alert("GAMEOVER");
-            }
-            console.log(game.currentStage);
+document.onkeyup = function (event) {
+    var input = event.key;
+    var isValid = false;
+    for (var i = 0; i < alpha.length; i++){
+        if(event.key == alpha[i]){
+            isValid = true;
         }
+    }
+    if (isValid){
+        game.lettersGuessed.push(input);
+        game.checkerUserInput(input);
+    }else{
+        game.lettersGuessed.push(input);
+        game.errorsMade++;
+        game.chances += -1;
+    }
+}
 
+var game = {
+    wordBank: ["margarita", "beer", "tequila"],
+    word : [],
+    placeholder : [],
+    lettersGuessed : [],
+    chances : 0,
+    errorsMade : 0,
+    start: function () {
+        game.word = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
+        this.calculateChances();
+        this.initPlaceholder(this.word);
+        this.interval = setInterval(this.updateInts, 100);
+        
     },
-    updateStats: function () {
-        document.getElementById('chancesLeft').innerHTML = "Guesses left : " + game.chances;
-        document.getElementById('zhowManyCorrect').innerHTML = "How many correct : " + game.howManyCorrect;
-        document.getElementById('indexcheck').innerHTML = "Current index : " + game.indexx;
+    calculateChances : function () {
+        game.chances = Math.abs(this.word.length + (this.word.length - (this.word.length - 3)));
+    },
+    initPlaceholder: function (word) {
+        for (var i = 0; i < this.word.length; i++){
+            this.placeholder[i] = (false);
+        }
+    },
+    checkUserInput : function (input) {
+        var inWord;
+        for( var i = 0; i < this.word.length; i++){
+            if(input === this.word.charAt([i])) {
+                this.placeholder[i] = true;
+                inWord = true;
+            } else {
+                inWord = false;
+            }
+        }
+        if(!inWord){
+            this.chances--;
+            this.errorsMade;
+        }else{
+            this.chances--;
+        }
+        this.updateDOM();
+    },
+    updateDOM: function (){
+        var stringTrans="";
+        for (var i = 0; i < this.placeholder.length; i++){
+            if(this.placeholder[i]){
+                stringTrans += this.word.charAt([i]);
+            }else{
+                stringTrans += "_";
+            }
+        }
+        console.log(stringTrans);
+        document.getElementById('a').innerHTML = stringTrans;
+    },
+
+
+   updateInts: function () {
+        document.getElementById('chancesLeft').innerHTML = "Guesses left : " +  game.chances;
+        document.getElementById('errors').innerHTML = "Errors made : " + game.errorsMade;
+       // document.getElementById('indexcheck').innerHTML = "Current index : " + game.indexx;
         
 
     }
 
 }
-game.run();
-
 
